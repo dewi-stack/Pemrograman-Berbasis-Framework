ce208cb494f1ad9a792f951f7dc73e7e7ac05901
Dewi Laylaturrohmah <dewi.layr2705@gmail.com>
2023-02-20 19:23:14 +0700

    Component

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const root= document.getElementById("root");
 
-function Navbar(){
-    const linkHome = document.createElement("a");
-    linkHome.href = "#home";
-    linkHome.textContent = "Home";
-    linkHome.onclick = function(event){
+function link(props){
+    const link = document.createElement("a");
+    link.href = props.href;
+    link.textContent = props.label;
+    link.onclick = function(event){
         event.preventDefault();
-        const homeScreen = HomeScreen();
+        const component = props.Component();
         root.innerHTML = "";
-        root.append(homeScreen);
+        root.append(component);
         history.pushState(null, "", event.target.href);
     };
 
-    const linkAbout = document.createElement("a");
-    linkAbout.href = "#about";
-    linkAbout.textContent = "About";
-    linkAbout.onclick = function(event){
-        event.preventDefault();
-        const aboutScreen = AboutScreen();
-        root.innerHTML = "";
-        root.append(aboutScreen);
-        history.pushState(null, "", event.target.href);
-    };
+    return link;
+}
+
+function Navbar(){
+    const linkHome = link({
+        href: "#home",
+        label: "Home",
+        Component: HomeScreen,
+    });
+
+    const linkAbout = link({
+        href: "#about",
+        label: "About",
+        Component: AboutScreen,
+    });
 
     const div = document.createElement("div");
     div.append(linkHome);
@@ -31,16 +36,11 @@ function Navbar(){
 }
 
 function AboutScreen(){
-    const linkHome = document.createElement("a");
-    linkHome.href = "#home";
-    linkHome.textContent = "Kembali Ke Home";
-    linkHome.onclick = function(event){
-        event.preventDefault();
-        const homeScreen = HomeScreen();
-        root.innerHTML = "";
-        root.append(homeScreen);
-        history.pushState(null, "", event.target.href);
-    };
+    const linkHome = link({
+        href: "#home",
+        label: "Kembali ke Home",
+        Component: HomeScreen,
+    });
 
     const text = document.createElement("p");
     text.textContent = "Welcome to About";
